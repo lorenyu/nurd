c5ccf0c4b9353655bd1288211b3f19d52cb57040
Loren Yu <yuloren@gmail.com>
2010-10-26 08:15:22 +0200

    Forgot to add server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+var util = require('util');
+var http = require('http');
+var EventEngineHttpServer = require('./EventEngineHttpServer').EventEngineHttpServer;
+
+var log = util.puts;
+
+var EventEngine = require('./EventEngine.js').EventEngine;
+EventEngine.observeAll(function(event) {
+    log('server.js:received event:' + JSON.stringify(event));
+});
+
+var server = new EventEngineHttpServer();
+server.listen(8080);
+util.puts('Server running at http://localhost:8080/');
