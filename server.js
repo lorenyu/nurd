9f5467713d0b223c6cf007eed267a181ef634893
Loren Yu <yuloren@gmail.com>
2010-11-04 08:45:59 +0100

    debugging

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
-}).listen(8124, "127.0.0.1");
+}).listen(8125, "127.0.0.1");
 
 
 //server.listen(8080, "127.0.0.1");
