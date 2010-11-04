54766b35c36d4c133accf67a046c2c1bd4466c92
Loren Yu <yuloren@gmail.com>
2010-11-04 09:20:13 +0100

    debugging

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 var util = require('util');
 var http = require('http');
+var path = require('path');
 var EventEngineHttpServer = require('./EventEngineHttpServer').EventEngineHttpServer;
 
 var log = util.puts;
 
 // change directory to location of server.js (i.e. this file)
-var serverPath = process.argv[1];
-process.chdir(serverPath);
+var serverDir = path.dirname(process.argv[1]);
+process.chdir(serverDir);
 
 var EventEngine = require('./EventEngine.js').EventEngine;
 EventEngine.observeAll(function(event) {
