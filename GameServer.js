4153a95b93b9180a9afc5ea9ed3ac11945e86da6
Loren Yu <yuloren@gmail.com>
2010-11-05 20:23:49 +0100

    Now, if deck is empty, doesn't deal any more cards

--- GameServer.js
+++ GameServer.js
@@ -161,7 +161,9 @@ this.Game = function() {
                 for (var j = 0, m = this.cardsInPlay.length; j < m; j += 1) {
                     if (Card.equals(this.cardsInPlay[j], card)) {
                         if (this.cardsInPlay.length <= 12) { // replace the card if there are fewer than 12 cards
-                            this.cardsInPlay[j] = deck.drawCard();
+                            if (!deck.isEmpty()) {
+                                this.cardsInPlay[j] = deck.drawCard();
+                            }
                         } else { // if there are more than 12 cards in play just remove the card
                             this.cardsInPlay.splice(j, 1);
                         }
