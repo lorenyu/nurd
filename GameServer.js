5b94148f70c5dfd1458ba19d1835403b4c18d45f
Loren Yu <yuloren@gmail.com>
2010-11-05 09:26:28 +0100

    Finding a set when there are more than 12 cards in play now simply removes the cards rather than replacing them with 3 new cards

--- GameServer.js
+++ GameServer.js
@@ -111,10 +111,14 @@ this.Game = function() {
                 var card = cards[i];
                 for (var j = 0, m = this.cardsInPlay.length; j < m; j += 1) {
                     if (Card.equals(this.cardsInPlay[j], card)) {
+                        if (this.cardsInPlay.length <= 12) { // replace the card if there are fewer than 12 cards
+                            this.cardsInPlay[j] = deck.drawCard();
+                        } else { // if there are more than 12 cards in play just remove the card
+                            this.cardsInPlay.splice(j, 1);
+                        }
                         break;
                     }
                 }
-                this.cardsInPlay[j] = deck.drawCard();
             }
         }
         return true;
