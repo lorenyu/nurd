1f5b0d7ecf564adb611fa84bd3a195b8d841d9a1
Loren Yu <yuloren@gmail.com>
2013-03-30 12:17:35 -0700

    added DeckTest

---
+++ test/DeckTest.js
@@ -0,0 +1,22 @@
+var chai = require('chai');
+
+var Deck = require('../Deck.js').Deck;
+
+chai.should();
+
+describe('Deck', function() {
+    describe('#Deck()', function() {
+    })
+    describe('#shuffle()', function() {
+    })
+    describe('#drawCard()', function() {
+    })
+    describe('#drawSpecificCard()', function() {
+    })
+    describe('#isEmpty()', function() {
+    })
+    describe('#numCards()', function() {
+    })
+    describe('#addCard()', function() {
+    })
+})
