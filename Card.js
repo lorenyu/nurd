99345a315074ac95fb114662e96971f7d16b0685
Loren Yu <yuloren@gmail.com>
2013-03-30 02:30:33 -0700

    made Card.equals more robust

--- Card.js
+++ Card.js
@@ -2,6 +2,12 @@ this.Card = function(shape, color, shading, number) {
     this.attributes = [shape, color, shading, number];
 };
 this.Card.equals = function(cardA, cardB) {
+    if (!cardA && cardB) {
+        return false;
+    }
+    if (cardA && !cardB) {
+        return false;
+    }
     for (var i = 0; i < 4; i += 1) {
         if (cardA.attributes[i] !== cardB.attributes[i]) {
             return false;
