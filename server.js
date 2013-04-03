246e7c798af693905102a03e723c5ff874b31ea0
Loren Yu <yuloren@gmail.com>
2013-04-02 22:31:00 -0700

    refactored server.js includes

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
-var util = require('util'),
-    http = require('http'),
-    path = require('path'),
-    EventEngineHttpServer = require('./EventEngineHttpServer').EventEngineHttpServer;
-var Game = require('./GameServer.js').Game;
+var express = require('express')
+  , util = require('util')
+  , _ = require('underscore')
+  , routes = require('./routes')
+  , http = require('http')
+  , path = require('path')
+  , EventEngineHttpServer = require('./EventEngineHttpServer').EventEngineHttpServer
+  , Game = require('./GameServer.js').Game
+  , EventEngine = require('./EventEngine.js').EventEngine;
 
 var log = util.puts;
 
