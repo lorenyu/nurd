61e7a474a43d3f22699b50bd8a98360a448bf785
Loren Yu <yuloren@gmail.com>
2013-04-03 17:22:03 -0700

    copied over a bunch of jade files

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var routes = module.exports = {
     js: require('./js'),
     index: function(req, res) {
-        res.render('index');
+        res.render('index', {title: 'Tttrio - a fast real-time multiplayer pattern-matching game to hone your logical skills'});
     },
     game: function(req, res) {
         res.sendfile('views/game.html');
