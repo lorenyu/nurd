bcad3da19c488d55fcffa5b0a02fa24738997650
Loren Yu <yuloren@gmail.com>
2013-04-04 19:56:32 -0700

    changed tab spacing in ChatServer

--- ChatServer.js
+++ ChatServer.js
@@ -1,26 +1,26 @@
 this.ChatServer = function() {
-    var init = function(eventEngine) {
-        eventEngine.observe('client:chat:sendMsg', function(event) {
-            var sender = event.data.sender;
-            var msg = event.data.msg;
-            if (msg) {
-                if (typeof(sender) !== 'string') {
-                    sender = JSON.stringify(sender);
-                }
-                if (typeof(msg) !== 'string') {
-                    msg = JSON.stringify(sender);
-                    var allowedCharacters = /^[^\w.,;:\'\"?\/`~!@#$%^&*()-=_+ \[\]{}\\|]+$/ig;
-                    msg = msg.replace(/\</g, '&lt;').replace(/\>/g, '&gt;').replace(
-                            allowedCharaceters, '');
-                }
-                console.log(sender);
-                eventEngine.fire('server:chat:sendMsg', {    
-                    sender: sender,
-                    msg: msg
-                });
-            }
+  var init = function(eventEngine) {
+    eventEngine.observe('client:chat:sendMsg', function(event) {
+      var sender = event.data.sender;
+      var msg = event.data.msg;
+      if (msg) {
+        if (typeof(sender) !== 'string') {
+          sender = JSON.stringify(sender);
+        }
+        if (typeof(msg) !== 'string') {
+          msg = JSON.stringify(sender);
+          var allowedCharacters = /^[^\w.,;:\'\"?\/`~!@#$%^&*()-=_+ \[\]{}\\|]+$/ig;
+          msg = msg.replace(/\</g, '&lt;').replace(/\>/g, '&gt;').replace(
+              allowedCharaceters, '');
+        }
+        console.log(sender);
+        eventEngine.fire('server:chat:sendMsg', {  
+          sender: sender,
+          msg: msg
         });
-    };
+      }
+    });
+  };
 
-    init.apply(this, arguments);
+  init.apply(this, arguments);
 };
