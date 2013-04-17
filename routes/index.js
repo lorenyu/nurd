b30b9558f6d7ee5811470eee1a552b2fe53600d9
Loren Yu <yuloren@gmail.com>
2013-04-17 15:18:40 -0400

    show game list in nav, move nav to footer

--- routes/index.js
+++ routes/index.js
@@ -20,7 +20,8 @@ var routes = module.exports = {
     res.render('game', {
       title: 'Tttrio - a fast real-time multiplayer pattern-matching game to hone your logical skills',
       description: 'Play Tttrio free now. Face off against friends, family, and strangers in a skill-based game of quick perception and pattern matching. Find trios of cards to score points and win.',
-      game: game
+      game: game,
+      games: Game.list()
     });
   }
 };
