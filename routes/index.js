fb6c84b2ec8d880fcfed599625aa1892de3a2668
Loren Yu <yuloren@gmail.com>
2013-06-26 22:28:26 -0400

    Hiding multiple game stuff for now since I want to get instructions up and running

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,12 @@ var Game = require('../GameServer').Game;
 var routes = module.exports = {
   js: require('./js'),
   index: function(req, res) {
-    res.render('index', {
-      title: 'Tttrio - a fast real-time multiplayer pattern-matching game to hone your logical skills',
-      description: 'Play Tttrio free now. Face off against friends, family, and strangers in a skill-based game of quick perception and pattern matching. Find trios of cards to score points and win.',
-      games: Game.list()
-    });
+    res.redirect(302, '/game/1');
+    // res.render('index', {
+    //   title: 'Tttrio - a fast real-time multiplayer pattern-matching game to hone your logical skills',
+    //   description: 'Play Tttrio free now. Face off against friends, family, and strangers in a skill-based game of quick perception and pattern matching. Find trios of cards to score points and win.',
+    //   games: Game.list()
+    // });
   },
   masterGame: function(req, res) {
     res.sendfile('views/game.html');
